1ca810cbb245eab4649c3975b535f65efe117705
LeonelNieto <leonel.nieto@mabe.com.mx>
2024-03-04 16:03:22 -0600

    Upload project

---
+++ RX130/RX130.bat
@@ -0,0 +1,2 @@
+C:\Users\lnlmexid\SEGGER\JLink\jlink.exe -device R5F51306 -if FINE -speed 1000 -autoconnect 1 -CommandFile eraseRX130.jlink
+C:\Users\lnlmexid\SEGGER\JLink\jlink.exe -device R5F51306 -if FINE -speed 1000 -autoconnect 1 -CommandFile upload.jlink
---
+++ RX231/flash.bat
@@ -0,0 +1,2 @@
+C:\Users\lnlmexid\SEGGER\JLink\jlink.exe -device R5F52317 -if FINE -speed 1000 -autoconnect 1 -CommandFile erase.jlink
+C:\Users\lnlmexid\SEGGER\JLink\jlink.exe -device R5F52317 -if FINE -speed 1000 -autoconnect 1 -CommandFile upload.jlink
